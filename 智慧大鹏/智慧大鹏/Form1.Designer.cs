7c59bb41c81c4a0a469fd7fe1be52daa01e0024b
zc <3492912044@qq.com>
2024-08-20 10:56:41 +0800

    添加启停，获取设备数据

--- 智慧大鹏/智慧大鹏/Form1.Designer.cs
+++ 智慧大鹏/智慧大鹏/Form1.Designer.cs
@@ -29,12 +29,213 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series25 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series26 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series27 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series28 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cb_fs = new System.Windows.Forms.CheckBox();
+            this.cb_yq = new System.Windows.Forms.CheckBox();
+            this.cb_sd = new System.Windows.Forms.CheckBox();
+            this.cb_wd = new System.Windows.Forms.CheckBox();
+            this.btn_history = new System.Windows.Forms.Button();
+            this.btn_StartOrStop = new System.Windows.Forms.Button();
+            this.btn_setinterval = new System.Windows.Forms.Button();
+            this.ipt_interval = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btn_sendEmail = new System.Windows.Forms.Button();
+            this.ipt_email = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_fs
+            // 
+            this.cb_fs.AutoSize = true;
+            this.cb_fs.Location = new System.Drawing.Point(223, 55);
+            this.cb_fs.Name = "cb_fs";
+            this.cb_fs.Size = new System.Drawing.Size(48, 16);
+            this.cb_fs.TabIndex = 23;
+            this.cb_fs.Text = "风速";
+            this.cb_fs.UseVisualStyleBackColor = true;
+            // 
+            // cb_yq
+            // 
+            this.cb_yq.AutoSize = true;
+            this.cb_yq.Location = new System.Drawing.Point(151, 55);
+            this.cb_yq.Name = "cb_yq";
+            this.cb_yq.Size = new System.Drawing.Size(48, 16);
+            this.cb_yq.TabIndex = 22;
+            this.cb_yq.Text = "氧气";
+            this.cb_yq.UseVisualStyleBackColor = true;
+            // 
+            // cb_sd
+            // 
+            this.cb_sd.AutoSize = true;
+            this.cb_sd.Location = new System.Drawing.Point(82, 55);
+            this.cb_sd.Name = "cb_sd";
+            this.cb_sd.Size = new System.Drawing.Size(48, 16);
+            this.cb_sd.TabIndex = 21;
+            this.cb_sd.Text = "湿度";
+            this.cb_sd.UseVisualStyleBackColor = true;
+            // 
+            // cb_wd
+            // 
+            this.cb_wd.AutoSize = true;
+            this.cb_wd.Location = new System.Drawing.Point(13, 55);
+            this.cb_wd.Name = "cb_wd";
+            this.cb_wd.Size = new System.Drawing.Size(48, 16);
+            this.cb_wd.TabIndex = 20;
+            this.cb_wd.Text = "温度";
+            this.cb_wd.UseVisualStyleBackColor = true;
+            // 
+            // btn_history
+            // 
+            this.btn_history.Location = new System.Drawing.Point(704, 12);
+            this.btn_history.Name = "btn_history";
+            this.btn_history.Size = new System.Drawing.Size(82, 23);
+            this.btn_history.TabIndex = 19;
+            this.btn_history.Text = "历史查询";
+            this.btn_history.UseVisualStyleBackColor = true;
+            // 
+            // btn_StartOrStop
+            // 
+            this.btn_StartOrStop.Location = new System.Drawing.Point(574, 12);
+            this.btn_StartOrStop.Name = "btn_StartOrStop";
+            this.btn_StartOrStop.Size = new System.Drawing.Size(124, 23);
+            this.btn_StartOrStop.TabIndex = 18;
+            this.btn_StartOrStop.Text = "开始/停止";
+            this.btn_StartOrStop.UseVisualStyleBackColor = true;
+            this.btn_StartOrStop.Click += new System.EventHandler(this.btn_StartOrStop_Click);
+            // 
+            // btn_setinterval
+            // 
+            this.btn_setinterval.Location = new System.Drawing.Point(136, 12);
+            this.btn_setinterval.Name = "btn_setinterval";
+            this.btn_setinterval.Size = new System.Drawing.Size(135, 23);
+            this.btn_setinterval.TabIndex = 17;
+            this.btn_setinterval.Text = "设置时间间隔";
+            this.btn_setinterval.UseVisualStyleBackColor = true;
+            this.btn_setinterval.Click += new System.EventHandler(this.btn_setinterval_Click);
+            // 
+            // ipt_interval
+            // 
+            this.ipt_interval.Location = new System.Drawing.Point(13, 14);
+            this.ipt_interval.Name = "ipt_interval";
+            this.ipt_interval.Size = new System.Drawing.Size(117, 21);
+            this.ipt_interval.TabIndex = 16;
+            // 
+            // chart1
+            // 
+            chartArea7.AxisX.Title = "时间";
+            chartArea7.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Stacked;
+            chartArea7.AxisY.Title = "温度/湿度/氧气/风速";
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(0, 93);
+            this.chart1.Name = "chart1";
+            series25.BorderWidth = 5;
+            series25.ChartArea = "ChartArea1";
+            series25.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series25.Color = System.Drawing.Color.Red;
+            series25.Legend = "Legend1";
+            series25.Name = "温度";
+            series26.BorderWidth = 5;
+            series26.ChartArea = "ChartArea1";
+            series26.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series26.Color = System.Drawing.Color.Lime;
+            series26.Legend = "Legend1";
+            series26.Name = "湿度";
+            series27.BorderWidth = 5;
+            series27.ChartArea = "ChartArea1";
+            series27.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series27.Color = System.Drawing.Color.Blue;
+            series27.Legend = "Legend1";
+            series27.Name = "氧气";
+            series28.BorderWidth = 5;
+            series28.ChartArea = "ChartArea1";
+            series28.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series28.Color = System.Drawing.Color.Fuchsia;
+            series28.Legend = "Legend1";
+            series28.Name = "风速";
+            this.chart1.Series.Add(series25);
+            this.chart1.Series.Add(series26);
+            this.chart1.Series.Add(series27);
+            this.chart1.Series.Add(series28);
+            this.chart1.Size = new System.Drawing.Size(800, 357);
+            this.chart1.TabIndex = 24;
+            this.chart1.Text = "chart1";
+            // 
+            // btn_sendEmail
+            // 
+            this.btn_sendEmail.Location = new System.Drawing.Point(704, 51);
+            this.btn_sendEmail.Name = "btn_sendEmail";
+            this.btn_sendEmail.Size = new System.Drawing.Size(82, 23);
+            this.btn_sendEmail.TabIndex = 26;
+            this.btn_sendEmail.Text = "发邮件测试";
+            this.btn_sendEmail.UseVisualStyleBackColor = true;
+            // 
+            // ipt_email
+            // 
+            this.ipt_email.Location = new System.Drawing.Point(470, 53);
+            this.ipt_email.Name = "ipt_email";
+            this.ipt_email.Size = new System.Drawing.Size(228, 21);
+            this.ipt_email.TabIndex = 25;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_sendEmail);
+            this.Controls.Add(this.ipt_email);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.cb_fs);
+            this.Controls.Add(this.cb_yq);
+            this.Controls.Add(this.cb_sd);
+            this.Controls.Add(this.cb_wd);
+            this.Controls.Add(this.btn_history);
+            this.Controls.Add(this.btn_StartOrStop);
+            this.Controls.Add(this.btn_setinterval);
+            this.Controls.Add(this.ipt_interval);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox cb_fs;
+        private System.Windows.Forms.CheckBox cb_yq;
+        private System.Windows.Forms.CheckBox cb_sd;
+        private System.Windows.Forms.CheckBox cb_wd;
+        private System.Windows.Forms.Button btn_history;
+        private System.Windows.Forms.Button btn_StartOrStop;
+        private System.Windows.Forms.Button btn_setinterval;
+        private System.Windows.Forms.TextBox ipt_interval;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btn_sendEmail;
+        private System.Windows.Forms.TextBox ipt_email;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- 智慧大鹏/智慧大鹏/Form1.cs
+++ 智慧大鹏/智慧大鹏/Form1.cs
@@ -1,20 +1,97 @@
-﻿using System;
+﻿using Modbus.Device;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace 智慧大鹏
 {
     public partial class Form1 : Form
     {
+        // 基于MOdbus通讯的库
+        ModbusMaster master;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM2";
+            serialPort1.DataBits = 8;
+            serialPort1.BaudRate = 9600;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Parity = Parity.None;
+            serialPort1.Open();
+            master = ModbusSerialMaster.CreateRtu(serialPort1);
+            //master.ReadHoldingRegisters(1,0x0000,1);
+        }
+
+        // 设置间隔
+        private void btn_setinterval_Click(object sender, EventArgs e)
+        {
+            timer1.Interval =Convert.ToInt32(ipt_interval.Text);
+        }
+
+        /// <summary>
+        /// 开始停止，控制计时器启停
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_StartOrStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 获取当前时间
+            DateTime now = DateTime.Now;
+            string time = now.ToString("HH:mm:ss");
+
+            if (cb_wd.Checked)
+            {
+                // 获取设备的信息
+                ushort[] values = master.ReadHoldingRegisters(1, 0x0000, 1);
+
+                double tem = values[0] / 10;
+
+                // chart1 图表
+                // chart1.Series[0]  显示温度的序列
+                AddChartPoint(chart1.Series[0].Points,time,tem);
+            }
+
+            // 存储到数据库  时间  温度  湿度  风速  氧气 
+            
+        }
+
+        /// <summary>
+        /// 添加点的方法
+        /// </summary>
+        /// <param name="points">序列集合</param>
+        /// <param name="time">时间</param>
+        /// <param name="value">值</param>
+        private void AddChartPoint(DataPointCollection points,string time,object value)
+        {
+            points.AddXY(time,value);
+            if (points.Count >= 15)
+            {
+                points.RemoveAt(0);
+            }
+        }
+
     }
 }
